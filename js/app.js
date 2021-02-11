4adf462a38418ca474d02bccb13c56da651b76b6
max-i-mys <maximys6205@gmail.com>
2021-02-11 11:42:22 +0200

    change: prossecing photo album slider

--- js/app.js
+++ js/app.js
@@ -17,3 +17,24 @@ window.onscroll = function showHeader() {
 
 contSubBtn.onclick = () => contSubMenu.classList.add("active");
 consSubClose.onclick = () => contSubMenu.classList.remove("active");
+
+const galleryThumbs = new Swiper(".gallery-bottom", {
+	spaceBetween: 10,
+	slidesPerView: 5,
+	freeMode: true,
+	loop: true,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+});
+const galleryTop = new Swiper(".gallery-top", {
+	slidesPerView: 1,
+	centeredSlides: true,
+	loop: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	thumbs: {
+		swiper: galleryThumbs,
+	},
+});
