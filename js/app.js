4ce5dc3d1ee19f69bd54df67044414e4a5658ee8
max-i-mys <maximys6205@gmail.com>
2021-02-19 14:33:59 +0200

    change: added the footer-box__bottom block

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ burgerBtn.onclick = () =>
 		? burgerMenu.classList.add("active")
 		: burgerMenu.classList.remove("active");
 
-// window.onscroll = function showHeader() {
-// 	let header = document.querySelector("#burgerMenu");
-// 	if (window.pageYOffset > 100) {
-// 		burgerMenu.classList.remove("active");
-// 	}
-// 	if (window.pageYOffset > 300) {
-// 		burgerMenu.classList.add("active-fixed");
-// 	} else {
-// 		burgerMenu.classList.remove("active-fixed");
-// 	}
-// };
+window.onscroll = function showHeader() {
+	let header = document.querySelector("#burgerMenu");
+	if (window.pageYOffset > 100) {
+		burgerMenu.classList.remove("active");
+	}
+	if (window.pageYOffset > 300) {
+		burgerMenu.classList.add("active-fixed");
+	} else {
+		burgerMenu.classList.remove("active-fixed");
+	}
+};
 
 contSubBtn.onclick = () => contSubMenu.classList.add("active");
 consSubClose.onclick = () => contSubMenu.classList.remove("active");
